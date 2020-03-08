722fb6a3758fe2fb929fa933c01814d3c2120042
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-08 21:31:13 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,8 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+<<<<<<< HEAD
   def change
   create_table :students do |t| 
     t.string :name 
 end 
 end  
 end 
+=======
+end  
+>>>>>>> 87daca019a6b10abc7dd7fb2e0836f933e7d72b9
 
